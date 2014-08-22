3653a4b755f1ba2b57426da35c49c35aad37333d
Italo Cunha <cunha@dcc.ufmg.br>
2014-08-22 14:19:44 -0300

    initial import

---
+++ log.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+extern int errno;
+
+#include "cyc.h"
+#include "log.h"
+
+/*****************************************************************************
+ * static variables
+ ****************************************************************************/
+static unsigned log_verbosity = 0;
+static struct cyclic *cyc = NULL;
+
+static void log_error(const char *file, int line);
+
+/*****************************************************************************
+ * public function implementations
+ ****************************************************************************/
+void log_init(unsigned verbosity, const char *path,
+		unsigned nbackups, unsigned maxsize)
+{
+	if(cyc) return;
+	log_verbosity = verbosity;
+	cyc = cyc_init_filesize(path, nbackups, maxsize);
+	if(!cyc) log_error(__FILE__, __LINE__);
+}
+
+void log_destroy(void)
+{
+	if(!cyc) return;
+	log_verbosity = 0;
+	cyc_destroy(cyc);
+	cyc = NULL;
+}
+
+void log_flush(void)
+{
+	if(!cyc) return;
+	cyc_flush(cyc);
+}
+
+void logd(unsigned int verbosity, const char *fmt, ...)
+{
+	if(!cyc) return;
+	va_list ap;
+	if(verbosity > log_verbosity) return;
+	va_start(ap,fmt);
+	if(!cyc_vprintf(cyc, fmt, ap)) log_error(__FILE__, __LINE__);
+	va_end(ap);
+}
+
+void loge(unsigned verbosity, const char *file, int lineno)
+{
+	if(!cyc) return;
+	if(verbosity > log_verbosity) return;
+	if(!errno) return;
+	if(!cyc_printf(cyc, "%s:%d: strerror: %s\n", file, lineno,
+			strerror(errno))) log_error(__FILE__, __LINE__);
+	errno = 0;
+}
+
+void logea(const char *file, int lineno, const char *msg)
+{
+	if(!cyc) exit(EXIT_FAILURE);
+	int myerrno = errno;
+	if(!cyc_printf(cyc, "%s:%d: aborting\n", file, lineno)) {
+		log_error(__FILE__, __LINE__);
+	}
+	if(msg) {
+		if(!cyc_printf(cyc, "%s:%d: %s\n", file, lineno, msg)) {
+			log_error(__FILE__, __LINE__);
+		}
+	}
+	errno = myerrno;
+	loge(0, file, lineno);
+	exit(EXIT_FAILURE);
+}
+
+int log_true(unsigned verbosity)
+{
+	return verbosity <= log_verbosity;
+}
+
+/*****************************************************************************
+ * static function implementations
+ ****************************************************************************/
+static void log_error(const char *file, int line)
+{
+	if(errno) perror("log_error");
+	fprintf(stderr, "%s:%d: logging not working.\n", file, line);
+}
---
+++ log.h
@@ -0,0 +1,55 @@
+/* This module creates a unique log handler for a program.  It supports several
+ * helper functions to handle logging of error messages.  This module uses the
+ * cyclic file handle module (cyc) to create thread-safe rotating files.  The
+ * interface is as follows:
+ *
+ * (1) initialize the log handler using =log_init=
+ * (2) print messages to the file using =logd=, =loge=, and =logea=
+ * (3) destroy the logging handler with =log_destroy= when you are done.
+ *
+ * This code is copyrighted by Italo Cunha (cunha@dcc.ufmg.br) and released
+ * under the latest version of the GPL. */
+
+#ifndef __LOG_HEADER__
+#define __LOG_HEADER__
+
+#include <inttypes.h>
+
+#define LOG_FATAL 1
+#define LOG_DEBUG 5
+#define LOG_WARN 50
+#define LOG_INFO 100
+#define LOG_EXTRA 1000
+
+/* This function initializes the global logger.  The parameter =verbosity=
+ * specifies what gets printed; calls to =logd=, =loge=, and =logea= with lower
+ * =verbosity= values will print messages.  The variable =prefix= controls the
+ * log files prefix (can include an absolute path).  The =nbackups= and
+ * =maxsize= specify the number of rotating log files and their maximum size,
+ * respectively. */
+void log_init(unsigned verbosity, const char *prefix, unsigned nbackups,
+		unsigned maxsize);
+
+void log_destroy(void);
+void log_flush(void);
+
+/* This function functions like printf and logs a message if its =verbosity= is
+ * lower than that passed to =log_init=. */
+void logd(unsigned verbosity, const char *fmt, ...);
+
+/* This functions prints an error message (built with strerror) if =ernno= is
+ * set and =verbosity= is lower than that passed to =log_init=.  It should be
+ * called with the file name and line number where the error occurred (use the
+ * __FILE__ and __LINE__ macros). */
+void loge(unsigned verbosity, const char *file, int lineno);
+
+/* Like loge, except it always prints the error message if =errno= is set.
+ * This function also prints =msg= and calls =exit= to end the program. */
+void logea(const char *file, int lineno, const char *msg)
+	__attribute__((noreturn));
+
+/* This function returns a nonzero value if its =verbosity= value is lower than
+ * that passed to =log_init=. */
+int log_true(unsigned verbosity);
+
+#endif
